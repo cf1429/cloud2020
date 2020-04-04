0c7399505bd1475d04eb9d469931a43bafe9915b
cf1429 <1429043932@qq.com>
2020-04-04 14:53:00 +0800

    配置了eureka注册中心，并且将模拟支付服务注入到了eureka注册中心

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,12 +2,13 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer  //表示注册中心
+public class EurekaMain7001 {
     public static void main(String[] args){
-        SpringApplication.run(PaymentMain8001.class);
+        SpringApplication.run(EurekaMain7001.class,args);
+
     }
 }
