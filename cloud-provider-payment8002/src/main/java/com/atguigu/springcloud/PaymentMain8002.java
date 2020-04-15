209bd616c440ba2c9e78dc0a5675171551d8457f
chenfengfeng <chenfengfeng123>
2020-04-15 10:02:41 +0800

    添加Payment8002支付服务

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8002 {
+
+    public static void main(String[] args){
+        SpringApplication.run(PaymentMain8002.class,args);
+
+    }
+}
