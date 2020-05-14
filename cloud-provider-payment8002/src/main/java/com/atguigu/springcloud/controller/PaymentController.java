ac37b95008a066854526d1979109f397aa037b76
cf1429 <1429043932@qq.com>
2020-05-14 23:42:32 +0800

    搭建支付服务集群8001 8002

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,13 +14,15 @@ import javax.annotation.Resource;
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
+    @Value("${server.port}")
+    private String serverPort;
 
     @PostMapping(value = "/payment/create")
     public CommonResult<Payment> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("插入结果。。。。"+result);
         if(result >0){
-            return new CommonResult(200,"插入数据库成功",result);
+            return new CommonResult(200,"插入数据库成功serverPort="+serverPort,result);
         }else{
             return new CommonResult(404,"插入数据库失败",null);
         }
@@ -33,7 +36,7 @@ public class PaymentController {
 
         int age = 0;
         if(payment != null ){
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功serverPort="+serverPort,payment);
         }else{
             return new CommonResult(404,"没有相应记录",null);
         }
