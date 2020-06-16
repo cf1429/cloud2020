8118d5cbabde467277792c1d4677dff926d0b281
cf1429 <1429043932@qq.com>
2020-06-16 23:37:39 +0800

    openFeign实现接口调用

---
+++ cloud-cosumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author cf
+ * @create 2020\6\16 0016
+ */
+@Slf4j
+@RestController
+public class OrderFeignController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> findPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.findPaymentById(id);
+
+    }
+}
