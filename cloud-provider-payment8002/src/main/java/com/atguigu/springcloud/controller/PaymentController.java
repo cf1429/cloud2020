f0f622bb40c6e5e3ecc17144e3ea5e9451eef9af
chenfengfeng <chenfengfeng123>
2020-11-06 13:37:56 +0800

    添加rabbitmq 代码

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -22,46 +22,47 @@ public class PaymentController {
     @Resource
     private DiscoveryClient discoveryClient;
 
-    @PostMapping(value = "/payment/create")
-    public CommonResult<Payment> create(@RequestBody Payment payment){
-        int result = paymentService.create(payment);
-        log.info("插入结果。。。。"+result);
-        if(result >0){
-            return new CommonResult(200,"插入数据库成功serverPort="+serverPort,result);
-        }else{
-            return new CommonResult(404,"插入数据库失败",null);
-        }
-
-    }
-
-    @GetMapping(value = "/payment/get/{id}")
-    public CommonResult findPaymentById(@PathVariable(value = "id") long id){
-        Payment payment = paymentService.findPaymentById(id);
-        log.info("获取结果。。。。"+payment);
-
-        int age = 0;
-        if(payment != null ){
-            return new CommonResult(200,"查询成功serverPort="+serverPort,payment);
-        }else{
-            return new CommonResult(404,"没有相应记录",null);
-        }
-    }
-
-    @GetMapping(value = "/payment/discovery")
-    public Object discovery(){
-        List<String> services = discoveryClient.getServices();
-        for(String element : services){
-            log.info("element:"+element);
-        }
-        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-        for(ServiceInstance serviceInstance : instances){
-            log.info(serviceInstance.getServiceId()+"\t"+serviceInstance.getInstanceId()+"\t"+serviceInstance.getPort());
-
-        }
-        return this.discoveryClient;
-
-
-    }
+//    @PostMapping(value = "/payment/create")
+//    public CommonResult<Payment> create(@RequestBody Payment payment){
+//        int result = paymentService.create(payment);
+//
+//        log.info("插入结果。。。。"+result);
+//        if(result >0){
+//            return new CommonResult(200,"插入数据库成功serverPort="+serverPort,result);
+//        }else{
+//            return new CommonResult(404,"插入数据库失败",null);
+//        }
+//
+//    }
+//
+//    @GetMapping(value = "/payment/get/{id}")
+//    public CommonResult findPaymentById(@PathVariable(value = "id") long id){
+//        Payment payment = paymentService.findPaymentById(id);
+//        log.info("获取结果。。。。"+payment);
+//
+//        int age = 0;
+//        if(payment != null ){
+//            return new CommonResult(200,"查询成功serverPort="+serverPort,payment);
+//        }else{
+//            return new CommonResult(404,"没有相应记录",null);
+//        }
+//    }
+//
+//    @GetMapping(value = "/payment/discovery")
+//    public Object discovery(){
+//        List<String> services = discoveryClient.getServices();
+//        for(String element : services){
+//            log.info("element:"+element);
+//        }
+//        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+//        for(ServiceInstance serviceInstance : instances){
+//            log.info(serviceInstance.getServiceId()+"\t"+serviceInstance.getInstanceId()+"\t"+serviceInstance.getPort());
+//
+//        }
+//        return this.discoveryClient;
+//
+//
+//    }
 
     @GetMapping(value = "/payment/lb")
     public String getPaymentLB(){
