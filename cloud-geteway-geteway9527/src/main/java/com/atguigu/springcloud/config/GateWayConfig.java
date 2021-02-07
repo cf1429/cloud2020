980877a48cac63b47c2296eb21213683e8979570
chenfengfeng <chenfengfeng123>
2021-02-07 15:13:43 +0800

    添加通过gateway 跳转到b站的操作

--- cloud-geteway-geteway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
+++ cloud-geteway-geteway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -10,14 +10,33 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class GateWayConfig {
+
+    /**
+     * 配置一个id为route-name的路由规则
+     * 当访问地址http://localhost:9527/guonei时会跳转转发到地址：http://news.baidu.com/guonei
+     * @param routeLocatorBuilder
+     * @return
+     */
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
         routes.route("path_route_atguigu1",r -> r.path("/guonei")
                 .uri("http://news.baidu.com/guonei")).build();
-
         return routes.build();
+    }
 
+    /**
+     * 跳转到b站
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_atguigu2",r-> r.path("/anime")
+                .uri("https://www.bilibili.com/anime")).build();
+        return routes.build();
     }
 
 }
+
