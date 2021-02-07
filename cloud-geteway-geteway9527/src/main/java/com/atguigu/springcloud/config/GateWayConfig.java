4654c21b575664464d352d6bd1ef88b382fe96f6
chenfengfeng <chenfengfeng123>
2021-02-07 11:11:53 +0800

    修改正确的gateway

--- cloud-geteway-geteway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
+++ cloud-geteway-geteway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 路由模式网关配置
+ */
 @Configuration
 public class GateWayConfig {
     @Bean
