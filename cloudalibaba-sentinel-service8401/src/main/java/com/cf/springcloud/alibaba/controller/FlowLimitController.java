480d93e773cf3d0e404b6de40dfbf5599f50d84f
cf1429 <1429043932@qq.com>
2021-03-02 00:29:56 +0800

    1、参照官网安装sentinel 2、创建sentinel测试项目 3、配置8041项目注册进nacos及配置sentinel监控

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/cf/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,17 @@
+package com.cf.springcloud.alibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA(){
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "-----testB";
+    }
+}
