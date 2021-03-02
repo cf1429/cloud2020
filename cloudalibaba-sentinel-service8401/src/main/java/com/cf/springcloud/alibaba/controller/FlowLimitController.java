e0bbca59e50d978652626751c9c0ff433cd4f2d6
cf1429 <1429043932@qq.com>
2021-03-03 00:35:14 +0800

    sentinel qps，线程数测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/cf/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/cf/springcloud/alibaba/controller/FlowLimitController.java
@@ -3,10 +3,17 @@ package com.cf.springcloud.alibaba.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "------testA";
     }
 
