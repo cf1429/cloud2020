177577903d2c1251a8a945240b1cc0395019d4cc
cf1429 <1429043932@qq.com>
2021-03-06 11:08:22 +0800

    sentinel 热点key 相关demo

--- cloudalibaba-sentinel-service8401/src/main/java/com/cf/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/cf/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,24 +1,62 @@
 package com.cf.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA(){
-        try {
-            TimeUnit.MILLISECONDS.sleep(800);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(800);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return "------testA";
     }
 
     @GetMapping("/testB")
     public String testB(){
+        log.info(Thread.currentThread().getName()+"\t-----testB");
         return "-----testB";
     }
+
+    @GetMapping("/testD")
+    public String testD(){
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        int age = 10/0;
+        log.info("test 测试异常比例");
+        return "------testD";
+    }
+
+    /**
+     * 热点key配置
+     * @param p1
+     * @param p2
+     * @return
+     */
+    @GetMapping(value = "/testHotKey")
+    @SentinelResource(value = "testHotKey" ,blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "------testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException blockException){
+        return "------deal_testHotKey";
+
+    }
+
+
 }
