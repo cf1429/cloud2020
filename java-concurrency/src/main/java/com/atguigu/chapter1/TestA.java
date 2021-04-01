68e30e44cbce8eb4f0e55dbe1a476462fa920d6c
cf1429 <1429043932@qq.com>
2021-04-01 23:31:12 +0800

    shiro 的第一个helloword

---
+++ java-concurrency/src/main/java/com/atguigu/chapter1/TestA.java
@@ -0,0 +1,21 @@
+package com.atguigu.chapter1;
+
+import java.io.IOException;
+import java.util.*;
+
+public class TestA extends HashMap<String,Object> {
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap  = new HashMap<>();
+        hashMap.put("六一","222");
+        hashMap.put("六34","222");
+        hashMap.put("六3","222");
+        hashMap.put("六55","222");
+        System.out.println(hashMap);
+
+
+
+    }
+
+
+
+}
---
+++ shiro/src/main/java/com/cf/shiro/TestAuthenticator.java
@@ -0,0 +1,46 @@
+package com.cf.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.subject.Subject;
+
+public class TestAuthenticator {
+    public static void main(String[] args) {
+        //创建安全管理器对象
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+
+        // 给安全管理器设置realm
+        IniRealm realm = new IniRealm("classpath:shiro.ini");
+        defaultSecurityManager.setRealm(realm);
+
+        //SecurityUtils 给全局安全工具类设置安全管理器
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+
+        // 关键对象 subject 主体
+        Subject subject = SecurityUtils.getSubject();
+
+        //创建令牌
+        UsernamePasswordToken token = new UsernamePasswordToken("xiaochen","1253");
+        try{
+            System.out.println("认证状态："+subject.isAuthenticated());
+            subject.login(token);   //用户认证
+            System.out.println("认证状态："+subject.isAuthenticated());
+        }catch (UnknownAccountException e){
+            e.printStackTrace();
+            System.out.println("用户不存在");
+        }catch (IncorrectCredentialsException e){
+            e.printStackTrace();
+            System.out.println("用户密码不正确");
+
+        }
+
+
+
+
+    }
+}
