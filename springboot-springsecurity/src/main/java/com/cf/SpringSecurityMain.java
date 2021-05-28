7b8ed23167c20da6a1f0e610f7d0ddd09506bfea
cf1429 <1429043932@qq.com>
2021-05-28 15:17:51 +0800

    关闭springsecurity安全认证

--- springboot-springsecurity/src/main/java/com/cf/SpringSecurityMain.java
+++ springboot-springsecurity/src/main/java/com/cf/SpringSecurityMain.java
@@ -2,13 +2,14 @@ package com.cf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * @Author:chenf
  * @Date:2021/5/28 11:19
  * @describe
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})  // 排除security安全验证
 public class SpringSecurityMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityMain.class,args);
