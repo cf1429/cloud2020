84ac698dd3c3a33df246912b690ff12e343fc235
cf1429 <1429043932@qq.com>
2021-05-28 18:07:56 +0800

    springsecurity添加方法级别的角色限制，对应的角色只能访问他拥有权限的接口

--- springboot-springsecurity/src/main/java/com/cf/config/MyWebSecurity.java
+++ springboot-springsecurity/src/main/java/com/cf/config/MyWebSecurity.java
@@ -3,6 +3,7 @@ package com.cf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,26 +14,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @Date:2021/5/28 15:24
  * @describe
  */
+@EnableGlobalMethodSecurity(prePostEnabled = true)  //启动角色认证
 @Configuration
 @EnableWebSecurity   //启用security安全框架的安全校验
 public class MyWebSecurity extends WebSecurityConfigurerAdapter {
     //在方法中配置用户名和密码，作为用户登录的数据
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 给用户添加角色
         PasswordEncoder pe = passwordEncoder();
-
         auth.inMemoryAuthentication()
                 .withUser("zhangsan")
                 .password(pe.encode("123"))
-                .roles();
+                .roles("admin","normal");
         auth.inMemoryAuthentication()
                 .withUser("lisi")
                 .password(pe.encode("lisi"))// 对密码进行加密
-                .roles();
+                .roles("normal");
         auth.inMemoryAuthentication()
                 .withUser("王五")
                 .password(pe.encode("123"))
-                .roles();
+                .roles("admin");
     }
 
     //创建密码的加密类
--- springboot-springsecurity/src/main/java/com/cf/controller/HelloSecurity.java
+++ springboot-springsecurity/src/main/java/com/cf/controller/HelloSecurity.java
@@ -1,5 +1,6 @@
 package com.cf.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,20 @@ public class HelloSecurity {
     @RequestMapping("/sayHello")
     public String  sayHello(){
         return "hello security";
+    }
+
+    //指定角色是admin，normal的用户可以访问
+    @RequestMapping("/helloUser")
+    @PreAuthorize(value = "hasAnyRole('admin','normal')")
+    public String helloUser(){
+        return "Hello 拥有normal，admin角色用户";
+    }
+
+    //角色是admin的用户可以访问
+    @RequestMapping("/helloAdmin")
+    @PreAuthorize(value = "hasAnyRole('admin')")
+    public String helloAdmin(){
+        return "Hello 拥有admin角色的用户可以访问";
+
     }
 }
