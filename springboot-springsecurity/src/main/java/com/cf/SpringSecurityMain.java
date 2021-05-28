1780baa2984f5db35c9a417d97ff89a424dd152c
cf1429 <1429043932@qq.com>
2021-05-28 11:32:03 +0800

    初始化springboot+springsecurity框架项目结构

---
+++ springboot-springsecurity/src/main/java/com/cf/SpringSecurityMain.java
@@ -0,0 +1,16 @@
+package com.cf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:chenf
+ * @Date:2021/5/28 11:19
+ * @describe
+ */
+@SpringBootApplication
+public class SpringSecurityMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMain.class,args);
+    }
+}
---
+++ springboot-springsecurity/src/main/java/com/cf/controller/HelloSecurity.java
@@ -0,0 +1,18 @@
+package com.cf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:chenf
+ * @Date:2021/5/28 11:27
+ * @describe
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloSecurity {
+    @RequestMapping("sayHello")
+    public String  sayHello(){
+        return "hello security 安全框架";
+    }
+}
