b12a477d65bf097287012a2758b9105a8d6415d6
cf1429 <1429043932@qq.com>
2021-07-07 23:11:50 +0800

    添加jwt的依赖及编写jwt的第一个测试程序

--- springboot-jwt/src/test/java/com/cf/SpringbootJwtApplicationTests.java
+++ springboot-jwt/src/test/java/com/cf/SpringbootJwtApplicationTests.java
@@ -1,13 +1,34 @@
 package com.cf;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class SpringbootJwtApplicationTests {
 
     @Test
     void contextLoads() {
+        Calendar instance = Calendar.getInstance();
+        instance.add(Calendar.SECOND,20);
+
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        String sign = JWT.create()
+                .withHeader(map)
+                .withClaim("userId", 1)   //payload
+                .withClaim("username", "xiaochen")
+                .withExpiresAt(instance.getTime())   // 过期时间
+                .sign(Algorithm.HMAC256("kjkjk@"));// 签名
+
+        System.out.println(sign);
+
+
     }
 
 }
