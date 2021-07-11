7c261cd979d64dcd12aadea8e5d9135d4826b3d2
cf1429 <1429043932@qq.com>
2021-07-11 16:52:20 +0800

    添加jwt解码代码

--- springboot-jwt/src/test/java/com/cf/SpringbootJwtApplicationTests.java
+++ springboot-jwt/src/test/java/com/cf/SpringbootJwtApplicationTests.java
@@ -1,7 +1,9 @@
 package com.cf;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,13 +13,11 @@ import java.util.Map;
 
 @SpringBootTest
 class SpringbootJwtApplicationTests {
-
+    // 生成token
     @Test
     void contextLoads() {
         Calendar instance = Calendar.getInstance();
-        instance.add(Calendar.SECOND,20);
-
-
+        instance.add(Calendar.SECOND,100);
         Map<String,Object> map = new HashMap<String,Object>();
         String sign = JWT.create()
                 .withHeader(map)
@@ -25,10 +25,16 @@ class SpringbootJwtApplicationTests {
                 .withClaim("username", "xiaochen")
                 .withExpiresAt(instance.getTime())   // 过期时间
                 .sign(Algorithm.HMAC256("kjkjk@"));// 签名
-
         System.out.println(sign);
-
-
+    }
+    // 验证token
+    @Test
+    public void test(){
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256("kjkjk@")).build();
+        DecodedJWT verify = verifier.verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MjU5OTMzODgsInVzZXJJZCI6MSwidXNlcm5hbWUiOiJ4aWFvY2hlbiJ9.Et2ujYCt2d3s83SfaY965Q7yvC1AbVTGfY-pdW0GFfQ");
+        System.out.println(verify.getClaims().get("userId").asInt());
+        System.out.println(verify.getClaims().get("username").asString());
+        System.out.println("过期时间："+verify.getExpiresAt());
     }
 
 }
